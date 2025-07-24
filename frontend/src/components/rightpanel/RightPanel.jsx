a344dd87701de990e1c3b0c22e65fa110b1f475e
Rahul <rahulp00rtx@gmail.com>
2025-07-24 19:52:45 +0530

    Created the follow and unfollow

--- frontend/src/components/rightpanel/RightPanel.jsx
+++ frontend/src/components/rightpanel/RightPanel.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeleton/RightPanelSkeleton";
 // import RightPanelSkeleton from "../skeleton/RightPanelSkeleton";
 import { useQuery } from "@tanstack/react-query";
+import useFollow from "../../hooks/useFollow";
+import LoadingSpinner from "../common/LoadingSpinner";
 
 const RightPanel = () => {
   // const isLoading = false;
@@ -22,6 +24,8 @@ const RightPanel = () => {
       }
     },
   });
+
+  const { follow, isPending } = useFollow();
   if (suggestedUser?.length == 0) return <div className="md:w-64 w-0"></div>;
   return (
     <div className="hidden lg:block my-4 mx-2">
@@ -62,9 +66,12 @@ const RightPanel = () => {
                 <div>
                   <button
                     className="btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm"
-                    onClick={(e) => e.preventDefault()}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      follow(user._id);
+                    }}
                   >
-                    Follow
+                    {isPending ? <LoadingSpinner size="sm" /> : "Follow"}
                   </button>
                 </div>
               </Link>
---
+++ frontend/src/hooks/useFollow.jsx
@@ -0,0 +1,35 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+const useFollow = () => {
+  const queryClient = useQueryClient();
+  const { mutate: follow, isPending } = useMutation({
+    mutationFn: async (userId) => {
+      try {
+        const res = await fetch(`/api/users/follow/${userId}`, {
+          method: "POST",
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error);
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+      Promise.all([
+        queryClient.invalidateQueries({ queryKey: ["suggestedUser"] }),
+        queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+      ]);
+      toast.success("Followed successfully");
+    },
+    onError: () => {
+      toast.error("Something went wrong");
+    },
+  });
+  return { follow, isPending };
+};
+
+export default useFollow;
